d86c30740376a61125cb672fe58eb917044d6db5
Andy Sisinger <andocmdo@gmail.com>
2018-01-07 15:14:21 -0500

    add routes for worker API endpoint

--- handlers.go
+++ handlers.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -26,7 +22,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 */
 
-// FrontEnd returns files in the working directory
+// Index returns files in the working directory
 func Index(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	file := vars["file"]
@@ -44,116 +40,4 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, string(data))
-}
-
-// JobIndex gets all jobs as JSON
-func JobIndex(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(jobs); err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-	}
-}
-
-// JobShow attemps to get a specific job based on ID
-func JobShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	jobID, err := strconv.Atoi(vars["jobID"])
-	if err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusBadRequest)
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-		return
-	}
-	job, err := RepoFindJob(jobID)
-	if err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusNotFound)
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-		return
-	}
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(job); err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-	}
-}
-
-// JobCreateJSON creates a job from JSON POST data to /jobs endpoint
-func JobCreateJSON(w http.ResponseWriter, r *http.Request) {
-	var job Job
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, uploadLimit))
-	if err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusBadRequest)
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-		return
-	}
-	if err := r.Body.Close(); err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusBadRequest)
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-		return
-	}
-	if err := json.Unmarshal(body, &job); err != nil {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(http.StatusUnprocessableEntity) // unprocessable entity
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-		return
-	}
-	job.Recieved = time.Now()
-	job.Valid = true
-	j := RepoCreateJob(job)
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(j); err != nil {
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-	}
-}
-
-// JobCreateURLEnc creates a job from JSON POST data to /jobs endpoint
-func JobCreateURLEnc(w http.ResponseWriter, r *http.Request) {
-	//job := Job{}
-	job := NewJob()
-
-	if err := r.ParseForm(); err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		log.Printf("no symbol in urlencoded form")
-		fmt.Fprintln(w, "error parsing form values")
-		return
-	}
-	for key, values := range r.PostForm {
-		job.Args[key] = values[0] // only using the first occurence of the parameter
-	}
-
-	if (job.Args["symbol"] == "") || (job.Args["startDate"] == "") || (job.Args["endDate"] == "") {
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		log.Printf("no symbol in urlencoded form")
-		fmt.Fprintln(w, "no symbol in urlencoded form")
-		return
-	}
-
-	job.Recieved = time.Now()
-	job.Valid = true
-	j := RepoCreateJob(*job)
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(j); err != nil {
-		log.Printf(err.Error())
-		fmt.Fprintln(w, err.Error())
-	}
 }
---
+++ workerHandlers.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// WorkerIndex gets all workers as JSON
+func WorkerIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(workers); err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+	}
+}
+
+// WorkerShow attemps to get a specific worker based on ID
+func WorkerShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	workerID, err := strconv.Atoi(vars["workerID"])
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+	worker, err := RepoFindWorker(workerID)
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusNotFound)
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(worker); err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+	}
+}
+
+// WorkerCreateJSON creates a worker from JSON POST data to /workers endpoint
+func WorkerCreateJSON(w http.ResponseWriter, r *http.Request) {
+	var worker Worker
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, uploadLimit))
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+	if err := r.Body.Close(); err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+	if err := json.Unmarshal(body, &worker); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusUnprocessableEntity) // unprocessable entity
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+	worker.Created = time.Now()
+	worker.Valid = true
+	wrkr := RepoCreateWorker(worker)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(wrkr); err != nil {
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+	}
+}
+
+// WorkerCreateURLEnc creates a worker from JSON POST data to /workers endpoint
+func WorkerCreateURLEnc(w http.ResponseWriter, r *http.Request) {
+	//worker := Worker{}
+	worker := NewWorker()
+
+	if err := r.ParseForm(); err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		log.Printf("error parsing worker form in urlencoded form")
+		fmt.Fprintln(w, "error parsing worker form values")
+		return
+	}
+
+	worker.Created = time.Now()
+	worker.Valid = true
+	wrkr := RepoCreateWorker(*worker)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(wrkr); err != nil {
+		log.Printf(err.Error())
+		fmt.Fprintln(w, err.Error())
+	}
+}
