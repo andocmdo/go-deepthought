ec4210bc7051cff1c64209692f0b5d084a7dd26f
Andy Sisinger <andocmdo@gmail.com>
2018-01-07 22:16:21 -0500

    fix logic in dealing jobs to workers. correctly dispatches jobs

--- dealer.go
+++ dealer.go
@@ -28,43 +28,44 @@ func init() {
 // Dealer depyloys jobs to waiting workers
 func dealer(d int, jobChan <-chan int, workerChan <-chan int) {
 	log.Printf("started dealer %d", d)
-	for jobID := range jobChan {
-		for workerID := range workerChan {
-			log.Printf("dealer %d is sending job %d to worker %d", d, jobID, workerID)
-			job, err := RepoFindJob(jobID)
-			if err != nil {
-				log.Printf("dealer %d encountered an error finding job %d to send to worker %d", d, jobID, workerID)
-				log.Printf(err.Error())
-			}
-
-			/* TODO these are update by the worker itself through api
-			job.Running = true
-			job.Started = time.Now()
-			_, err = RepoUpdateJob(job)
-			if err != nil {
-				log.Printf("error on job %d", jobID)
-				log.Printf(err.Error())
-			}
-			*/
-
-			// This is where we send out job
-			// connect to tcp port and send job data
-			log.Printf("Dealer %d sent job %d to worker %d ", d, jobID, workerID)
-
-			// And when finished, note the time, check for errors, etc
-			job.Dispatched = true
-			/* TODO These will be updated by the API
-			job.Running = false
-			job.Completed = true
-			job.Result = string(out)
-			*/
-			_, err = RepoUpdateJob(job)
-			if err != nil {
-				log.Printf("error dispatching job %d", jobID)
-				log.Printf(err.Error())
-				return
-			}
-			log.Printf("dealer %d successfully dispatched job %d to worker %d", d, jobID, workerID)
+	for {
+		workerID := <-workerChan
+		jobID := <-jobChan
+		log.Printf("dealer %d is sending job %d to worker %d", d, jobID, workerID)
+		job, err := RepoFindJob(jobID)
+		if err != nil {
+			log.Printf("dealer %d encountered an error finding job %d to send to worker %d", d, jobID, workerID)
+			log.Printf(err.Error())
 		}
+
+		/* TODO these are update by the worker itself through api
+		job.Running = true
+		job.Started = time.Now()
+		_, err = RepoUpdateJob(job)
+		if err != nil {
+			log.Printf("error on job %d", jobID)
+			log.Printf(err.Error())
+		}
+		*/
+
+		// This is where we send out job
+		// connect to tcp port and send job data
+		log.Printf("Dealer %d sent job %d to worker %d ", d, jobID, workerID)
+
+		// And when finished, note the time, check for errors, etc
+		job.Dispatched = true
+		/* TODO These will be updated by the API
+		job.Running = false
+		job.Completed = true
+		job.Result = string(out)
+		*/
+		_, err = RepoUpdateJob(job)
+		if err != nil {
+			log.Printf("error dispatching job %d", jobID)
+			log.Printf(err.Error())
+			return
+		}
+		log.Printf("dealer %d successfully dispatched job %d to worker %d", d, jobID, workerID)
+
 	}
 }
