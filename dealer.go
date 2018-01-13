97e021087127856424531753dab18637436de538
Andy Sisinger <andocmdo@gmail.com>
2018-01-13 17:30:47 -0500

    add zmq reqeust

--- dealer.go
+++ dealer.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+
+	zmq "github.com/pebbe/zmq4"
 )
 
 const jobQueueSize = 10000
@@ -13,7 +16,6 @@ var readyWorkers chan int
 func init() {
 	jobsToRun = make(chan int, jobQueueSize)
 	readyWorkers = make(chan int, workerQueueSize)
-
 	log.Println("Job queue size: ", jobQueueSize)
 	log.Println("Worker queue size: ", workerQueueSize)
 
@@ -23,6 +25,7 @@ func init() {
 // Dealer depyloys jobs to waiting workers
 func dealer(d int, jobChan <-chan int, workerChan <-chan int) {
 	log.Printf("started dealer %d", d)
+
 	for {
 		workerID := <-workerChan
 		jobID := <-jobChan
@@ -32,28 +35,26 @@ func dealer(d int, jobChan <-chan int, workerChan <-chan int) {
 			log.Printf("dealer %d encountered an error finding job %d to send to worker %d", d, jobID, workerID)
 			log.Printf(err.Error())
 		}
-
-		/* TODO these are update by the worker itself through api
-		job.Running = true
-		job.Started = time.Now()
-		_, err = RepoUpdateJob(job)
+		wrkr, err := RepoFindWorker(workerID)
 		if err != nil {
-			log.Printf("error on job %d", jobID)
+			log.Printf("dealer %d encountered an error finding worker %d", d, workerID)
 			log.Printf(err.Error())
 		}
-		*/
 
 		// This is where we send out job
 		// connect to tcp port and send job data
+		log.Printf("dealer %d has started sending job %d to worker %d", d, jobID, workerID)
+		requester, _ := zmq.NewSocket(zmq.REQ)
+		requester.Connect("tcp://" + wrkr.IPAddr + ":" + wrkr.Port)
+		requester.Send("hello", 0)
+		reply, _ := requester.Recv(0)
+		fmt.Printf("Received reply [%s]\n", reply)
+		requester.Close()
+
 		log.Printf("Dealer %d sent job %d to worker %d ", d, jobID, workerID)
 
 		// And when finished, note the time, check for errors, etc
 		job.Dispatched = true
-		/* TODO These will be updated by the API
-		job.Running = false
-		job.Completed = true
-		job.Result = string(out)
-		*/
 		_, err = RepoUpdateJob(job)
 		if err != nil {
 			log.Printf("error dispatching job %d", jobID)
